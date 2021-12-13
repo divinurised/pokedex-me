be56b9b8a11af31865e29488a3a259d7d64bae08
Davi Alcântara <dxvialcantara@gmail.com>
2021-12-13 12:48:51 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Homepage from './Pages/Homepage';
-import PokemonPage from './Pages/PokemonPage';
-import About from './Pages/About';
+import Homepage from './Pages/Homepage/index';
+import PokemonPage from './Pages/PokemonPage/index';
+import About from './Pages/About/index';
 
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
